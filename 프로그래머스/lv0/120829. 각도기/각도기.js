2304f8eda93213433687e880204b3d84e8f5c360
Freesian <68853713+freesian12@users.noreply.github.com>
2022-11-12 15:53:33 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,8 +1,5 @@
 function solution(angle) {
     
-    if(angle < 90) return 1;
-    else if(angle == 90) return 2;
-    else if(angle > 90 && angle < 180) return 3;
-    else return 4;
+    return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
 
 }
