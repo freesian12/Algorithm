0394cd98ff1045ae9ce3b573e3ac9ab9d4d66cab
Freesian <68853713+freesian12@users.noreply.github.com>
2022-11-15 17:10:55 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for(let i of my_string)
+        {
+            answer += i.repeat(n);
+        }
+        
+    return answer;
+}
