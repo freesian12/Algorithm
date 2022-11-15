8ac77d753a1955b4a6499ae17a906782c9fd2d67
Freesian <68853713+freesian12@users.noreply.github.com>
2022-11-15 15:12:55 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,15 @@
 function solution(my_string) {
+    //     let sum = "";
+    //     for(let i = my_string.length-1 ; )
+    //         {
+    //            sum += my_string[i]
+    //         }
+    //     return sum
     
-    return my_string.split("").reverse().join("");
+    // console.log(my_string.split(""));
+    // console.log(my_string.split("").reverse());
+    // console.log(my_string.split("").reverse().join(""));
+    return my_string.split("").reverse().join("")
+    
+    //[...my_string].reverse().join('');
 }
