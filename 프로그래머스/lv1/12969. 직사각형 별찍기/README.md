e6c9f4265c0a680fe44f3fde3600c81cefcbabf3
Freesian <68853713+freesian12@users.noreply.github.com>
2022-11-16 16:06:37 +0900

    [level 1] Title: 직사각형 별찍기, Time: 134.79 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
 
 ### 성능 요약
 
-메모리: 32.1 MB, 시간: 54.99 ms
+메모리: 37.4 MB, 시간: 134.79 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -4,8 +4,18 @@ process.stdin.on('data', data => {
     const a = Number(n[0]), b = Number(n[1]);
     // console.log(a); // 가로
     // console.log(b); // 세로
+    let answer = "";
+    //console.log(('*'.repeat(a)+'\n').repeat(b))
     
-    console.log(('*'.repeat(a)+'\n').repeat(b))
+    for(let i = 0; i < b; i++) // 세로 
+        {
+            for(let j = 0 ; j < a ; j++) // 가로
+                {
+                    answer += "*";
+                }
+            answer += '\n'
+        }
+    console.log(answer)
     
     
     
